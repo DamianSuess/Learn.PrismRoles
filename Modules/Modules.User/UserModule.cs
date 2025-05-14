161a8bafee259c31373d2b3215fd519d494d8be8
Damian Suess <damian.suess@omnicell.com>
2025-05-14 10:19:54 -0400

    Base project for prism roles (Prism 5.0)

---
+++ Modules/Modules.User/UserModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using Modules.User.Views;
+
+namespace Modules.User
+{
+    public class UserModule : IModule
+    {
+        IRegionManager _regionManager;
+        public UserModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(UserView));
+        }
+    }
+}
